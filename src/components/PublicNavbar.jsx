c44c0113d5544685f150146649de0a1946498079
omjaisatya <satyaprakash451256@gmail.com>
2024-07-13 14:07:53 +0530

    add some design

--- src/components/PublicNavbar.jsx
+++ src/components/PublicNavbar.jsx
@@ -1,10 +1,14 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
 const PublicNavbar = () => {
   return (
     <Navbar className="nav">
       <Nav className="text-center align-center">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/signup">Signup</NavLink>
       </Nav>
